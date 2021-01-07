1b44b51ef30c6dc5db470215dc1e3b7b4f6463bc
Om Rawal <omrawal007@gmail.com>
2021-01-07 21:11:23 +0530

    file saving enabled

--- driver_code.py
+++ driver_code.py
@@ -2,17 +2,26 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from steganography import Steganography
+import time
 
 steg_var=Steganography()
 
 mask_img=cv2.imread("./Resources/sherlock.jpg")
 secret_message_img=cv2.imread("./Resources/message.jpg")
 cypher=steg_var.embed_a_in_b(secret_message_img,mask_img)
+
+filename='./encrypted_images/'+time.strftime("%Y%m%d-%H%M%S")+'.jpg'
+print(cv2.imwrite(filename, cypher))
+
 cv2.imshow('mask',mask_img)
 cv2.imshow('secret message',secret_message_img)
 cv2.imshow('cypher image',cypher)
 cv2.waitKey()
 
 decyphered_image=steg_var.decrypt_image(cypher)
+
+filename='./decrypted_images/'+time.strftime("%Y%m%d-%H%M%S")+'.jpg'
+print(cv2.imwrite(filename, decyphered_image))
+
 cv2.imshow('Decrypted image',decyphered_image)
 cv2.waitKey()
